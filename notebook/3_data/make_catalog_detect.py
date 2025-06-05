1b63e7a0673d5d06218cb88ea1d341649846474b
Xiangchong Li <mr.superonion@hotmail.com>
2025-06-06 01:23:27 +0900

    commit

--- notebook/3_data/make_catalog_detect.py
+++ notebook/3_data/make_catalog_detect.py
@@ -15,11 +15,15 @@ from xlens.process_pipe.anacal_detect import (AnacalDetectPipe,
 
 # Parse command-line arguments
 def parse_args():
-    parser = argparse.ArgumentParser(description="Process patch masks with MPI.")
+    parser = argparse.ArgumentParser(
+        description="Process patch masks with MPI."
+    )
     parser.add_argument(
         "--start", type=int, required=True, help="Start index of datalist."
     )
-    parser.add_argument("--end", type=int, required=True, help="End index of datalist.")
+    parser.add_argument(
+        "--end", type=int, required=True, help="End index of datalist."
+    )
     return parser.parse_args()
 
 
@@ -27,14 +31,15 @@ def parse_args():
 def split_work(data, size, rank):
     return data[rank::size]
 
+
 def process_patch(entry, skymap, task):
     tract_id = entry["tract"]
     patch_db = entry["patch"]
     patch_x = patch_db // 100
     patch_y = patch_db % 100
     patch_id = patch_x + patch_y * 9
-    db_dir = "/lustre/work/xiangchong.li/work/hsc_s23b_data/catalogs/database/"
-    outdir = f"{db_dir}/s23b-anacal/tracts/{tract_id}/{patch_id}"
+    bdir = "/lustre/work/xiangchong.li/work/hsc_s23b_data/catalogs/database/"
+    outdir = f"{bdir}/s23b-anacal/tracts/{tract_id}/{patch_id}"
     out_fname = os.path.join(outdir, "detect.fits")
     if os.path.isfile(out_fname):
         return
@@ -52,10 +57,16 @@ def process_patch(entry, skymap, task):
         return
     fname = files[0]
     exposure = afwImage.ExposureF.readFits(fname)
-    base_dir = "/lustre/work/xiangchong.li/work/hsc_s23b_data/catalogs/database/"
-    mask_dir = f"{base_dir}/s23b-brightStarMask/tracts_mask/{tract_id}/{patch_id}"
+    mask_dir = f"{bdir}/s23b-brightStarMask/tracts_mask/{tract_id}/{patch_id}"
     mask_fname = os.path.join(mask_dir, "mask.fits")
     bmask = fitsio.read(mask_fname)
+
+    sp_dir = f"{bdir}/s23b-brightGalaxyMask/tracts/{tract_id}/{patch_id}"
+    sp_fname = os.path.join(sp_dir, "catalog.fits")
+    if os.path.isfile(sp_fname):
+        spg = fitsio.read(sp_fname)
+    else:
+        spg = None
 
     seed = tract_id * 1000 + patch_id
     data = task.anacal.prepare_data(
@@ -68,9 +79,10 @@ def process_patch(entry, skymap, task):
         tract=tract_id,
         patch=patch_id,
         star_mask_array=bmask,
+        star_cat=spg,
     )
     catalog = task.anacal.run(**data)
-    sel = catalog["is_primary"]
+    sel = (catalog["is_primary"]) & (catalog["mask_value"] < 30)
     del data, exposure, wcs, bbox, bmask
     fitsio.write(out_fname, catalog[sel])
     del catalog, sel
